ea1adf938317da38170c4f69ca90c8b1acda7639
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-27 15:25:02 +0800

    code clean

---
+++ frontend/mybek/components/breadcrumb.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ChevronRightIcon from './icons/chevron-right';
+
+function Breadcrumb() {
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <a
+            href="#"
+            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <div className="flex items-center">
+            <ChevronRightIcon />
+            <a
+              href="#"
+              className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+            >
+              Projects
+            </a>
+          </div>
+        </li>
+        <li aria-current="page">
+          <div className="flex items-center">
+            <ChevronRightIcon />
+            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+              Flowbite
+            </span>
+          </div>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Breadcrumb;
--- frontend/mybek/pages/index.js
+++ frontend/mybek/pages/index.js
@@ -1,3 +1,10 @@
+import Breadcrumb from '@/components/Breadcrumb';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Breadcrumb />
+      <h1>Home</h1>
+    </>
+  );
 }
