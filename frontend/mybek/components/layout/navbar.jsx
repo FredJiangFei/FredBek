1467814b27209816da731640b4dda2f333255ffa
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-27 21:04:26 +0800

    code clean

--- frontend/mybek/components/layout/navbar.jsx
+++ frontend/mybek/components/layout/navbar.jsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import Menu from '../icons/menu';
 import SearchIcon from '../icons/search';
 import Button from '../ui/button';
 import { useEffect, useState } from 'react';
+import classes from './navbar.module.css';
 
 const Navbar = () => {
   const [color, setColor] = useState('transparent');
+  const topHeader = '60px';
 
   useEffect(() => {
     const changeColor = () => {
-      if (window.scrollY >= 90) {
+      if (window.scrollY >= 30) {
         setColor('#ffffff');
       } else {
         setColor('transparent');
@@ -21,39 +22,41 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="bg-gray-300 p-4 flex container fixed">
-        <span className="flex-1"></span>
-        <input
-          type="text"
-          name="search"
-          placeholder="Search"
-          className="px-5 py-3 text-gray-800 w-0 border-0 rounded-full 
-                focus:outline-0 focus:w-96 duration-500"
-        />
-        <SearchIcon />
-      </div>
       <div
-        style={{
-          backgroundImage: 'url(/slider.jpeg)',
-          backgroundSize: 'cover',
-          height: '500px',
-        }}
+        className="bg-gray-300 flex container fixed items-center"
+        style={{ height: topHeader }}
       >
+        <span className="flex-1"></span>
+        <div className="flex items-center">
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+            className="text-gray-800 w-36 h-10 border-0 rounded-full pl-4 
+                focus:outline-0 focus:w-96 duration-500"
+          />
+          <div className="absolute right-4">
+            <SearchIcon />
+          </div>
+        </div>
+      </div>
+      <div className={classes.slider}>
         <div
-          className="flex items-center fixed top-20 w-full"
+          className="flex items-center fixed top-18 w-full"
           style={{
             backgroundColor: `${color}`,
+            top: topHeader,
           }}
         >
           <Link href="/">
             <Image src="/logo.png" alt="site logo" width={128} height={77} />
           </Link>
-          <ul className="flex md:flex-row space-x-8 flex-1 mx-4">
+          <ul className="flex space-x-8 flex-1 mx-4">
             <li>
-              <Link href="/Products">Products & Applications</Link>
+              <Link href="/products">Products & Applications</Link>
             </li>
             <li>
-              <Link href="/Sustainability">Sustainability</Link>
+              <Link href="/sustainability">Sustainability</Link>
             </li>
           </ul>
           <Button>Contact Us</Button>
---
+++ frontend/mybek/pages/sustainability.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sustainability() {
+  return <h1>Sustainability</h1>;
+}
+
+export default Sustainability;
